d4c82b556761b07fecfe2259a7e6afa9b62fd54a
roljs <roljs@hotmail.com>
2016-10-11 23:27:42 -0700

    Improved error messages and fixed logic to deal with different batch sizes returned by reddit. Also made UX improvements on progress UI and status.

--- RedditXLWeb/Home.js
+++ RedditXLWeb/Home.js
@@ -3,21 +3,12 @@
 (function () {
     "use strict";
 
-    var cellToHighlight;
     var authenticator;
     var processedRows = 0;
     var maxRows = 20000;
-    var rowCount;
     var templates = [];
-    var rowsToAdd = [];
     var batchSize = 1000;
-    var tableToggle;
     var insertAt = "A1";
-
-
-    $.getJSON("ApiTemplates.json", function (json) {
-        templates = json.templates;
-    });
 
     // The initialize function must be run each time a new page is loaded.
     Office.initialize = function (reason) {
@@ -26,38 +17,15 @@
         $(document).ready(function () {
             // Initialize the FabricUI notification mechanism and hide it
 
-            var DropdownHTMLElements = document.querySelectorAll('.ms-Dropdown');
-            for (var i = 0; i < DropdownHTMLElements.length; ++i) {
-                new fabric['Dropdown'](DropdownHTMLElements[i]);
-            }
-            var TextFieldElements = document.querySelectorAll(".ms-TextField");
-            for (var i = 0; i < TextFieldElements.length; i++) {
-                new fabric['TextField'](TextFieldElements[i]);
-            }
-            var spinners = document.querySelectorAll('.ms-Spinner');
-            for (var i = 0; i < spinners.length; i++) {
-                new fabric['Spinner'](spinners[i]);
-            }
-            $("#spinner").hide();
+            uiInit();
 
-            tableToggle = new fabric['Toggle'](document.querySelector('#tableToggle'));
-            /*
-            var messageBanners = document.querySelectorAll('.ms-MessageBanner');
-            for (var i = 0; i < messageBanners.length; i++) {
-                new fabric['MessageBanner'](messageBanners[i]);
-            }
-            */
+            $.ajaxSetup({
+                error: handleXhrError
+            });
 
-
-            $("#template-description").text("Specify how you want the data to be imported and click Get Data.");
-            $('#maxRows').val(localStorage.getItem("maxRows") ? localStorage.getItem("maxRows"): maxRows);
-            $('#subReddit').val(localStorage.getItem("subReddit"));
-            $('#insertAt').val("A1");
-            // Add a click event handler for the highlight button.
-            $('#getData').click(importData);
-            $('body').click(function () { $('#messageBar').hide(); });
-            $('#calloutAnchor').click(function (event) { $('#messageBar').toggle(); event.stopPropagation();});
-            $('#tableCheck').prop("checked", true);
+            $.getJSON("ApiTemplates.json", function (json) {
+                templates = json.templates;
+            });
 
             authenticator = new OfficeJSHelpers.Authenticator();
 
@@ -79,6 +47,37 @@
 
         });
     }
+
+    function uiInit() {
+        var DropdownHTMLElements = document.querySelectorAll('.ms-Dropdown');
+        for (var i = 0; i < DropdownHTMLElements.length; ++i) {
+            new fabric['Dropdown'](DropdownHTMLElements[i]);
+        }
+        var TextFieldElements = document.querySelectorAll(".ms-TextField");
+        for (var i = 0; i < TextFieldElements.length; i++) {
+            new fabric['TextField'](TextFieldElements[i]);
+        }
+        var spinners = document.querySelectorAll('.ms-Spinner');
+        for (var i = 0; i < spinners.length; i++) {
+            new fabric['Spinner'](spinners[i]);
+        }
+        $("#spinner").hide();
+
+        new fabric['Toggle'](document.querySelector('#tableToggle'));
+
+        $("#template-description").text("Specify how you want the data to be imported and click Get Data.");
+        $('#maxRows').val(localStorage.getItem("maxRows") ? localStorage.getItem("maxRows") : maxRows);
+        $('#subReddit').val(localStorage.getItem("subReddit"));
+        $('#tableCheck').prop("checked", true);
+        $('#insertAt').val("A1");
+
+        // Add a click event handlers
+        $('#getData').click(importData);
+        $('body').click(function () { $('#messageBar').hide(); });
+        $('#status').click(function (event) { $('#messageBar').toggle(); event.stopPropagation(); });
+
+    }
+
 
     function handleSelectionChange(args) {
 
@@ -106,17 +105,14 @@
         var qs = $("#options").val();
         var insertAt = $('#insertAt').val();
 
-        $('#totalRows').text("");
-        $('#spinner').show();
-
         processedRows = 0;
-        rowsToAdd = [];
 
         ga("send", "event", "Actions", "Clicked Import Data");
 
         //Initiate REST call execution from the service to load the data in batches
         authenticator.authenticate("Reddit")
             .then(function (token) {
+                $('#spinner').show();
                 $('#notificationBody').text("Executing...");
                 loadBatchOfData(token, api, subReddit, qs, "", batchSize, null, insertAt, insertAt);
 
@@ -128,7 +124,7 @@
         var url = "https://oauth.reddit.com/r/" + subReddit + api;
         //url = "https://www.reddit.com/r/survivor/api/flairlist.json";
 
-        rowCount = 0;
+        var rowsAdded = 0;
         var baseUrl = url;
 
         url = baseUrl + "?limit=" + batchSize;
@@ -153,62 +149,26 @@
 
             Excel.run(function (ctx) {
 
-                /*
-                var batchMode = false;
-                if (Office.context.requirements.isSetSupported('ExcelApi', 1.4)) {
-                    // If ExcelApi 1.4 is supported, then we can bulk add rows - first to an array, and then to the table.
-                    batchMode = true;
-                }
-
-                if (watermark == "") { //First batch, create the table
-
-                    //Create the table
-                    var tableR1C1 = getTableR1C1("Sheet1", template.headers.length);
-                    var table = ctx.workbook.tables.add(tableR1C1, true);
-                    table.getHeaderRowRange().values = [template.headers];
-                    table.name = "RedditTable";
-                    addRowsToTable(table, response, template, true);
-                    console.log("AddRows");
-                    return ctx.sync();
-
-                } else { //subsequent batch
-                    /*
-                    var table = ctx.workbook.tables.getItem("RedditTable");
-                    return ctx.sync().then(function (data) {
-                        addRowsToTable(table, response, template, true);
-                        table.getRange().getEntireColumn().format.autofitColumns();
-                        table.getRange().getEntireRow().format.autofitRows();
-                        console.log("AddRows");
-                        return ctx.sync();
-                    });
-
-                }
-
-                */
-
                 var sheet = ctx.workbook.worksheets.getActiveWorksheet();
-                var r = addRowsAsRange(sheet, startCellR1C1, response, template);
+                rowsAdded = addRowsAsRange(sheet, startCellR1C1, response, template);
 
-                console.log(r + "rows added");
+                console.log(rowsAdded + "rows added");
                 return ctx.sync();
 
-
-
             }).then(function (data) { //Excel run then
-                processedRows += rowCount;
+                processedRows += rowsAdded;
                 var nextBatchId = getNextBatchId(response, template);
 
                 if (nextBatchId && processedRows < maxRows) {
                     $('#spinner').show();
-                    $('#rows').text(processedRows + " rows. Max is " + maxRows);
+                    $('#status').text(processedRows + " rows...");
 
-                    var newStartCellR1C1 = startCellR1C1.match(/\D+/) + (parseInt(startCellR1C1.match(/\d+/)) + batchSize);
+                    var newStartCellR1C1 = startCellR1C1.match(/\D+/) + (parseInt(startCellR1C1.match(/\d+/)) + rowsAdded);
                     loadBatchOfData(token, api, subReddit, options, nextBatchId, batchSize, template, newStartCellR1C1, initialCellR1C1);
                 } else {
                     $('#notificationBody').text("Idle.");
                     $('#spinner').hide();
-                    $('#rows').text("");
-                    $('#totalRows').text(processedRows + " total rows imported.");
+                    $('#status').text(processedRows + " total rows imported.");
 
                     if ($('#tableCheck').hasClass("is-selected")) {
                         Excel.run(function (ctx) {
@@ -232,7 +192,7 @@
 
                 console.log(processedRows + " rows processed so far.");
             }).catch(errorHandler); //Excel run catch
-        }).fail(errorHandler);
+        });
     }
 
     function getNextBatchId(response, template) {
@@ -348,60 +308,19 @@
         return rows;
     }
 
-    function addRowsToTable(table, response, template, batchMode) {
-
-        rowsToAdd = [];
-
-        var rows = getRowsNode(response, template); //TODO: Get rows according to template instead of hardcoded to .data.children
-        rows.forEach(function (item) {
-
-            var values = [];
-            if (template.dataNode) item = item[template.dataNode];
-
-            for (var count = 0; count < template.headers.length; count++) {
-                var value = item[template.props[count]];
-                if (typeof value != "undefined") {
-                    switch (template.types[count]) {
-                        case "epochDate":
-                            values.push(epochSecsToDateString(value));
-                            break;
-                        case "number":
-                            values.push(value);
-                            break;
-                        default:
-                            values.push(JSON.stringify(value));
-                    }
-                } else {
-                    console.log("Item doesn't have property " + template.props[count]);
-                    values.push("undefined");
-                }
-            }
-
-            if (batchMode) {
-                // In batch mode (ExcelApi 1.4 API must be supported), then we can bulk add rows - first to an array, and then to the table.
-                rowsToAdd.push(values);
-            }
-            else {
-                // Rows must be added to the table one at a time 
-                table.rows.add(null, [values]);
-            }
-            rowCount++;
-            $('#spinner').show();
-            $('#rows').text(processedRows + rowCount + " rows. Max is " + maxRows);
-
-        });
-        if (batchMode) {
-            table.rows.add(null, rowsToAdd);
-        }
-    }
 
     function addRowsAsRange(sheet, startCell, response, template) {
 
-        rowsToAdd = [];
-
+        var rowCount;
+        var rowsToAdd = [];
 
         var rows = getRowsNode(response, template);
-        rows.forEach(function (item) {
+
+        for (rowCount = 0; rowCount < rows.length; rowCount++) {
+            if (processedRows + rowCount >= maxRows)
+                break;
+
+            var item = rows[rowCount];
 
             var values = [];
             if (template.dataNode) item = item[template.dataNode];
@@ -426,12 +345,11 @@
             }
 
             rowsToAdd.push(values);
-            rowCount++;
 
-            $('#spinner').show();
-            $('#rows').text(processedRows + rowCount + " rows. Max is " + maxRows);
+            $('#status').text(processedRows + rowCount + " rows...");
 
-        });
+
+        }
 
 
         var r1c1 = startCell + ":" + getColumnNameFromIndex(getIndexFromColumnName(startCell.match(/\D+/)[0]) + template.headers.length - 1) + (parseInt(startCell.match(/\d+/)) + rowCount - 1);
@@ -449,9 +367,9 @@
 
     function getIndexFromColumnName(columnName) {
         var index = 0;
-        for (var i = 0; i< columnName.length; i++) {
+        for (var i = 0; i < columnName.length; i++) {
 
-            index += (columnName.toUpperCase().charCodeAt(i) - 64) * Math.pow(26,i);
+            index += (columnName.toUpperCase().charCodeAt(i) - 64) * Math.pow(26, i);
         }
 
         return index;
@@ -494,28 +412,37 @@
 
     }
 
+    function handleXhrError(xhr, errorType, exceptionThrown) {
+        var errorMsg;
+        if (xhr.status == 0) {
+            errorMsg = "Either that subreddit doesn't exist or you're not connected"
+        } else {
+            errorMsg = "Something went wrong connecting to Reddit <br/><b>" + xhr.status + ": " + xhr.statusText + "</b>";
+        }
+        showNotification("Reddit Error", errorMsg);
+
+    }
 
     // Helper function for treating errors
     function errorHandler(error) {
-        ga("send", "event", "Actions", "Error");
 
-        $("#spinner").hide();
         // Always be sure to catch any accumulated errors that bubble up from the Excel.run execution
-        var errorMsg = JSON.stringify(error);
-        console.log("Error: " + error);
+        var errorMsg = "Excel didn't like that!<br/><b>" + error + "</b>";
         if (error instanceof OfficeExtension.Error) {
-            console.log("Debug info: " + JSON.stringify(error.debugInfo));
-            errorMsg += "<br/>" + JSON.stringify(error.debugInfo);
+            errorMsg += "<br/>" + error.debugInfo.errorLocation;
         }
-        $('#totalRows').text(processedRows + " total rows imported.");
 
-        showNotification("Error", errorMsg);
+        showNotification("Office Error", errorMsg);
     }
 
     // Helper function for displaying notifications
     function showNotification(header, content) {
-        $("#notificationHeader").text(header);
-        $("#notificationBody").text(content);
-        $("#messageBar").show();
+        ga("send", "event", "Actions", header);
+        $("#spinner").hide();
+        $('#status').text(processedRows + " total rows imported.");
+        console.log("Error: " + content);
+
+        $("#notificationBody").html("<h2>" + header + "</h2>" + content);
+        $("#messageBar").slideDown();
     }
 })();
